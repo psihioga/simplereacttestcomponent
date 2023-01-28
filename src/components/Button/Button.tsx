bd071fdcd3be8f6809576d9c52fee73e8478eda8
Psihioga <damirtahautdinov@gmail.com>
2023-01-28 23:41:15 +0300

    added storybook and tailwind

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "./Button.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  const style = `bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`;  
+  return <button className={style}>{props.label}</button>;
 };
 
 export default Button;
